c7df8f110b075b5995c1fa9f42666331956672df
sebastian8080 <sebas25211@hotmail.com>
2023-12-20 10:45:48 -0500

    agregando modal de imagenes

--- resources/views/web/show-property.blade.php
+++ resources/views/web/show-property.blade.php
@@ -228,7 +228,7 @@
 
     <div class="modal fade" id="modalImages" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-lg">
-            <div class="modal-content bg-transparent">
+            <div class="modal-content bg-transparent border-0">
                 <div class="modal-header bg-transparent">
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
