56da26c56763014ddf44f0a6319f912874645bba
sebastian8080 <sebas25211@hotmail.com>
2024-01-10 10:35:32 -0500

    subiendo favicon

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link rel="icon" href="{{asset('favicon3.png')}}" type="image/x-icon" />
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
