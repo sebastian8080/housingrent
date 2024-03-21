1d05944f476716347e2c3109278e4da9e6d46eea
sebastian8080 <sebas25211@hotmail.com>
2024-03-21 10:30:07 -0500

    upload admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\HomeController;
 
-Route::get('', [HomeController::class, 'index']);
+Route::get('/properties', [HomeController::class, 'index'])->name('properties.index');
+Route::get('/property/create', [HomeController::class, 'create'])->name('propertie.create');
+
+
+Route::get('/getcities/{state_id}', [HomeController::class, 'getcities'])->name('get.cities');
