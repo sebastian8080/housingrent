8dce000e68522d0ce523163ed1011a80c794d5fb
sebastian8080 <sebas25211@hotmail.com>
2024-04-16 15:53:58 -0500

    retirando texto de incluye alicuota, agua, luz, etc

--- resources/views/web/show-property.blade.php
+++ resources/views/web/show-property.blade.php
@@ -186,7 +186,7 @@
                     <div class="d-flex justify-content-center text-white py-3 shadow" style="background-color: #242B40; border-radius: 25px 25px 0px 0px">
                         <div class="d-flex justify-content-between gap-4">
                             <span style="font-size: xx-large" class="border-end pe-4 fw-bold">${{ $listing->property_price }}</span>
-                            <span style="font-size: medium">Incluye alícuota, <br> agua, luz e internet</span>
+                            
                         </div>
                     </div>
                     <div class="bg-white pt-4 pb-5 shadow px-5" style="border-radius: 0px 0px 25px 25px">
